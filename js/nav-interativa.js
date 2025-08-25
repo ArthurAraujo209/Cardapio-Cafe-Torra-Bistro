1a80a696cf611dfa2d76e922a7959a52f0abc913
Arthur Araujo <arthurlimaaraujo209@gmail.com>
2025-08-24 23:14:35 -0300

    Nova Responsividade na navbar e novas seções no cardápio

--- js/nav-interativa.js
+++ js/nav-interativa.js
@@ -19,3 +19,33 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+
+
+// Adiciona a funcionalidade de rolagem suave para o item ativo na navbar
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 2) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navItems.forEach(item => {
+    item.classList.remove("active");
+    if (item.getAttribute("href") === `#${current}`) {
+      item.classList.add("active");
+      
+      // Faz a navbar rolar pro item ativo
+      item.scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+        block: "nearest"
+      });
+    }
+  });
+});
